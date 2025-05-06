abf7ccc49df6956ea9b02b139479e29d6c5f7fac
bowy <149318982+shainakels@users.noreply.github.com>
2025-05-06 09:51:17 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,24 +3,27 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const signupRoute = require('./routes/signup');
 const loginRoute = require('./routes/login');
-const profileRoute = require('./routes/profile'); // Import the profile route
-const mixtapeRoute = require('./routes/mixtape'); // Import the mixtape route
+const profileRoute = require('./routes/profile'); // Import profile route
+const mixtapeRoute = require('./routes/mixtape');
+const authRoute = require('./routes/auth'); // Import auth route
+const forgotPasswordRoute = require('./routes/forgot-password'); // Import forgot-password route
+const resetPasswordRoute = require('./routes/reset-password'); // Import reset-password route
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
-// Root route
 app.get('/', (req, res) => {
   res.send('Welcome to the Harmonilink API!');
 });
 
-// Use routes
+// Use routes under /api
 app.use('/api', signupRoute);
-app.use('/api/login', loginRoute);
-app.use('/api', profileRoute); // Use the profile route
-app.use('/api', mixtapeRoute); // Register the mixtape route
-
+app.use('/api', loginRoute);
+app.use('/api', profileRoute); // Register profile route
+app.use('/api', mixtapeRoute);
+app.use('/api/auth', authRoute); // Register auth route
+app.use('/api/forgot-password', forgotPasswordRoute); // Register forgot-password route
+app.use('/api/reset-password', resetPasswordRoute); // Register reset-password route
 
 app.listen(3000, () => console.log('Server running on port 3000'));
