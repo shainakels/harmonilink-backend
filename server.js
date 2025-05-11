7882dba2d02d0cc7ad87a2f15ba06e17c629e95f
bowy <149318982+shainakels@users.noreply.github.com>
2025-05-11 20:59:46 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -9,13 +9,25 @@ const authRoute = require('./routes/auth'); // Import auth route
 const forgotPasswordRoute = require('./routes/forgot-password'); // Import forgot-password route
 const resetPasswordRoute = require('./routes/reset-password'); // Import reset-password route
 const discoverRoute = require('./routes/discover'); // Import discover route
+const discardRoute = require('./routes/discard'); // Import discard route
+const favoritesRoute = require('./routes/favorites'); // Import favorites route
 const errorHandler = require('./middleware/errorHandler'); // Import error handler middleware
 const https = require('https');
 const fs = require('fs');
-
+const path = require('path'); // Import path module
 const app = express();
-app.use(cors());
+const createMixtapeRoute = require('./routes/create-mixtape');
+
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(bodyParser.json());
+
+// Serve static files from the "uploads" directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Harmonilink API!');
@@ -30,6 +42,9 @@ app.use('/api/auth', authRoute); // Register auth route
 app.use('/api/forgot-password', forgotPasswordRoute); // Register forgot-password route
 app.use('/api/reset-password', resetPasswordRoute); // Register reset-password route
 app.use('/api', discoverRoute); // Register discover route
+app.use('/api', discardRoute); // Register discard route
+app.use('/api', favoritesRoute); // Register the favorites route
+app.use('/api', createMixtapeRoute); // Register mixtape route
 
 app.use(errorHandler); // Use error handler middleware
 
